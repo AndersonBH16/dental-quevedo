569fc0f9b59e1f3560d242d972b0ae9b98e35cd2
Anderson Blas <andi_1516@hotmail.com>
2022-09-23 00:21:56 -0500

    DQ-13: Crear controladores para Historial Clinico

--- resources/views/historial-clinico/historial.blade.php
+++ resources/views/historial-clinico/historial.blade.php
@@ -126,8 +126,256 @@
                         <textarea id="diagnostico_pre" class="form-control" placeholder="Escribir detalle" rows="5"></textarea><br>
                     </div>
                     <label class="">4. Antecedentes</label>
-                    <div class="form-group row"><br>
+                    <div class="form-group"><br>
                         <label class="">a. Personales</label>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="row">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            Hipertensión
+                                        </label>
+                                    </div>
+                                    <div class="form-check ml-3">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                                        <label class="form-check-label" for="flexCheckChecked">
+                                            Hepatitis
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            Diabetes
+                                        </label>
+                                    </div>
+                                    <div class="form-check ml-3">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                                        <label class="form-check-label" for="flexCheckChecked">
+                                            Anemia
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            Trastornos respiratorios
+                                        </label>
+                                    </div>
+                                    <div class="form-check ml-3">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                                        <label class="form-check-label" for="flexCheckChecked">
+                                            Epilepsia
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            Tuberculosis
+                                        </label>
+                                    </div>
+                                    <div class="form-check ml-3">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                                        <label class="form-check-label" for="flexCheckChecked">
+                                            Fiebre reumática
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            Enfermedad cardíaca
+                                        </label>
+                                    </div>
+                                    <div class="form-check ml-3">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                                        <label class="form-check-label" for="flexCheckChecked">
+                                            Trastornos gástricos
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            Enfermedad renal
+                                        </label>
+                                    </div>
+                                    <div class="form-check ml-3">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                                        <label class="form-check-label" for="flexCheckChecked">
+                                            Trastornos hormonales
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            Neuralgia
+                                        </label>
+                                    </div>
+                                    <div class="form-check ml-3">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                                        <label class="form-check-label" for="flexCheckChecked">
+                                            Alergia a algún medicamento
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            Infección venérea o SIDA
+                                        </label>
+                                    </div>
+                                    <div class="form-check ml-3">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                                        <label class="form-check-label" for="flexCheckChecked">
+                                            Colesterol
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            Artitris
+                                        </label>
+                                    </div>
+                                    <div class="form-check ml-3">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                                        <label class="form-check-label" for="flexCheckChecked">
+                                            Algún tumor
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="row">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            Enfermedad de la piel
+                                        </label>
+                                    </div>
+                                    <div class="form-check ml-3">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                                        <label class="form-check-label" for="flexCheckChecked">
+                                            Embarazo
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            Discrasias sanguíneas
+                                        </label>
+                                    </div>
+                                    <div class="form-check ml-3">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                                        <label class="form-check-label" for="flexCheckChecked">
+                                            Trastornos psicológicos
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            Fuma
+                                        </label>
+                                    </div>
+                                    <div class="form-check ml-3">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                                        <label class="form-check-label" for="flexCheckChecked">
+                                            Alcoholismo
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            Drogadicción
+                                        </label>
+                                    </div>
+                                    <div class="form-check ml-3">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                                        <label class="form-check-label" for="flexCheckChecked">
+                                            Cáncer
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            Otra enfermedad
+                                        </label>
+                                    </div>
+                                    <div class="form-check ml-3">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                                        <label class="form-check-label" for="flexCheckChecked">
+                                            Toma algún medicamento
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="form-check">
+                                        <input class="for
+
+                                        m-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            Quirúrgicos
+                                        </label>
+                                    </div>
+                                    <div class="form-check ml-3">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                                        <label class="form-check-label" for="flexCheckChecked">
+                                            Quimioterapia o Radioterapia
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label>Ampliación</label><br>
+                                    <input id="ampliacion" type="text" class="form-control" placeholder="Ampliación">
+                                </div>
+                            </div>
+                        </div>
+                        <label class="">b. Familiares</label>
+                        <div class="">
+                            <textarea id="familiares" class="form-control" placeholder="Descripción" rows="5"></textarea>
+                        </div>
+                        <label class="">c. Estomatológicos</label>
+                        <div class="row">
+                            <div class="col-md-8">
+                                <label class="form-check-label" for="flexCheckDefault">
+                                    ¿Controla su salud bucal con visitas periódicas al dentista?
+                                </label>
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                <div class="form-check ml-3">
+                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                                    <label class="form-check-label" for="flexCheckChecked">
+                                        Embarazo
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+
+                            </div>
+
+
+
+
+                        </div>
                     </div>
                 </div>
             </div>
